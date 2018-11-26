5fdbf8166014c5beb0170cc265811fc501fd0b25
Conor Hamilton <conhamil19@gmail.com>
2018-11-26 21:42:03 +0000

    working on authentication

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,10 +1,23 @@
 class UserController < ApplicationController 
   
   get '/signup' do 
-    erb :"users/signup"
+   if !logged_in?
+      erb :"/users/signup"
+   else
+      redirect :"/restaurants"
+    end 
   end 
   
   post '/signup' do 
+     if 
+       params[:username] == "" || params[:password] == ""
+       redirect "/signup"
+     else
+       @user = User.create(username: params[:username], password: params[:password])
+       @user.save
+       session[:user_id] = @user.id
+       redirect "/restaurants"
+     end 
   end 
   
   
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base 
+  has_secure_password
+  
   has_many :restaurants
   has_many :meals, :through => :restaurants 
 end 
