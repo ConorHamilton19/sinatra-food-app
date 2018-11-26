c82fa707ba4c71a5bcb743653a311461547d13cf
Conor Hamilton <conhamil19@gmail.com>
2018-11-26 19:35:25 +0000

    restaurant new view and controller

--- app/controllers/restaurant_controller.rb
+++ app/controllers/restaurant_controller.rb
@@ -10,8 +10,23 @@ class RestaurantController < ApplicationController
   end 
   
   get '/restaurants/new' do 
+   if logged_in?
+     erb :"/restaurants/new"
+   else 
+     redirect "/login"
+   end 
+  end 
+  
+  post '/restaurants' do 
     if logged_in?
-      erb :"/resaurants/new"
+      if params[:name] == ""
+        redirect "/restaurants/new"
+      else 
+        @restaurant = Restaurant.create(name: params[:name])
+        current_user.restaurant << @restaurant
+        @restaurant.save
+        redirect "/restaurant/#{@restaurant.id}"
+     end 
     else 
       redirect "/login"
     end 
